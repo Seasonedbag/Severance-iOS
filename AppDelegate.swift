7952fc6aab90ebf1175e89ae78f951ad0cc6ed66
Seasonedbag <tonkevin18@gmail.com>
2025-11-15 01:36:20 -0500

    Add files via upload

--- AppDelegate.swift
+++ AppDelegate.swift
@@ -1,10 +1,2 @@
 import UIKit
-
-@main
-class AppDelegate: UIResponder, UIApplicationDelegate {
-    func application(_ application: UIApplication,
-                     configurationForConnecting connectingSceneSession: UISceneSession,
-                     options: UIScene.ConnectionOptions) -> UISceneConfiguration {
-        return UISceneConfiguration(name: "Default Configuration", sessionRole: connectingSceneSession.role)
-    }
-}
+@main class AppDelegate: UIResponder, UIApplicationDelegate {}
